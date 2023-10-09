cb27310b35899d4d908d5cbae955012978ac8f5e
bigdeac <138499093+bigdeac@users.noreply.github.com>
2023-10-09 19:36:04 -0400

    Add files via upload
    To run, save this to a folder and open a PowerShell instance. Run by typing the location such as "C:\Scripts\Project1.ps1". This script uses the Get-WmiObject cmdlet to retrieve information about logical disks on the system. It then iterates through each drive, checking if it's a valid drive (not a CD-ROM) and adds up the free space. Finally, it outputs the total number of drives and the total free space in gigabytes.

---
+++ Project1.ps1
@@ -0,0 +1,19 @@
+﻿# Get all drives
+$drives = Get-WmiObject Win32_LogicalDisk
+
+# Initialize counters
+$driveCount = 0
+$totalFreeSpace = 0
+
+# Loop through each drive
+foreach ($drive in $drives) {
+    # Check if drive is ready and not a CD-ROM
+    if ($drive.DriveType -eq 3) {
+        $driveCount++
+        $totalFreeSpace += $drive.FreeSpace
+    }
+}
+
+# Output the results
+Write-Host "Number of drives: $driveCount"
+Write-Host "Total free space across all drives: $($totalFreeSpace / 1GB) GB"
