5c1e80f9c8c4c5a290ab38bc53be4c0c792b2f52
bigdeac <138499093+bigdeac@users.noreply.github.com>
2023-10-11 15:58:34 -0400

    Update sysinfo.ps1

--- sysinfo.ps1
+++ sysinfo.ps1
@@ -7,10 +7,10 @@ function getIP{
 $IP = getIP
 $User = $env:UserName
 $Ver = $PSVersionTable.PSVersion.Major
-$Host = $env:ComputerName
+$HostName = $env:ComputerName
 $Date = Get-Date -Format "dddd, MMMM dd, yyyy"
 
-$BODY = "This machine's IP is $IP. User is $User. Hostname is $Host. PowerShell $Ver. Today's Date is $Date."
+$BODY = "This machine's IP is $IP. User is $User. Hostname is $HostName. PowerShell $Ver. Today's Date is $Date."
 
 # Output $BODY to a file
 $BODY | Out-File -FilePath "C:\Scripts\output.txt"
