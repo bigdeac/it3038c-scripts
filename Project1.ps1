c0bfc3b253bb8d7cf1cfaa81e23e40c8553f8600
bigdeac <138499093+bigdeac@users.noreply.github.com>
2023-11-01 20:43:46 -0400

    Update Project1.ps1

--- Project1.ps1
+++ Project1.ps1
@@ -17,3 +17,29 @@ foreach ($drive in $drives) {
 #Output the results
 Write-Host "Number of drives: $driveCount"
 Write-Host "Total free space across all drives: $($totalFreeSpace / 1GB) GB"
+
+#Project 2 
+# Loop through each drive again
+foreach ($drive in $drives) {
+    # Check if drive is ready and not a CD-ROM
+    if ($drive.DriveType -eq 3) {
+        $driveLetter = $drive.DeviceID
+        $largestDir = ""
+        $largestSize = 0
+
+        # Get all directories in the drive
+        $dirs = Get-ChildItem -Path $driveLetter\ -Directory
+
+        # Loop through directories and find the largest one
+        foreach ($dir in $dirs) {
+            $dirSize = (Get-ChildItem -Path $dir.FullName -Recurse | Measure-Object -Property Length -Sum).Sum
+            if ($dirSize -gt $largestSize) {
+                $largestSize = $dirSize
+                $largestDir = $dir.FullName
+            }
+        }
+
+        # Output the largest directory for this drive
+        Write-Host "Drive $driveLetter - Largest Directory: $largestDir, Size: $($largestSize / 1GB) GB"
+    }
+}
